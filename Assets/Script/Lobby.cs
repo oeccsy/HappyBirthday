2373fc0273e9c48be5c56f3af0ca88f8e681ab8f
oeccsy <oeccsy@users.noreply.github.com>
2021-03-05 20:14:41 +0900

    Lobby Scene 추가
    - 게임 시작 전에 나타나는 로비 화면을 추가하였다.
    
    - Lobby.cs 로 중앙의 Text 선택시 게임 Scene으로 넘어가도록 구현하였다.

---
+++ Assets/Script/Lobby.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Lobby : MonoBehaviour
+{
+    float time = 0;
+
+    public Text txt;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if ((int)time % 2 == 1)
+        {
+            Debug.Log(time);
+            txt.text = "선물 받으러 가기";
+
+        }
+
+        if((int)time % 2 == 0)
+        {
+            txt.text = " ";
+        }
+            
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
